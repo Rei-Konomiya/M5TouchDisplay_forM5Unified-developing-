977728e168ba5116375ddc6e39eb5e8eecb3643c
Rei-Konomiya <reikonomiya56@gmail.com>
2025-01-05 14:50:29 +0900

    [fix]途中経過

--- src/VisualData.hpp
+++ src/VisualData.hpp
@@ -5,33 +5,24 @@
 #include <vector>
 
 enum class DrawType{
-  DrawPixel,
-  DrawFastVLine,
-  DrawFastHLine,
-  DrawRect,
-  DrawRoundRect,
-  DrawCircle,
-  DrawEllipse,
-  DrawLine,
-  DrawTriangle,
-  drawBezier,
-  drawEllipseArc,
-  drawArc,
-  
+  DrawPixel,      // ピクセル 描画
+  DrawLine,       // 直線 描画
+  DrawBezier,     // ベジェ曲線 描画
+  DrawArc,        // 正円弧 描画
+  DrawEllipseArc, // 楕円弧 描画
+  DrawRect,       // 矩形 描画
+  DrawRoundRect,  // 角丸矩形 描画
+  DrawCircle,     // 円形 描画
+  DrawEllipse,    // 楕円 描画
+  DrawTriangle,   // 三角形 描画
+  FillRect,       // 矩形 描画
+  FillRoundRect,  // 角丸矩形 描画
+  FillCircle,     // 円形 描画
+  FillEllipse,    // 楕円 描画
+  FillTriangle,   // 三角形 描画
+  FloodFill       // バケツ塗りつぶし
 };
 
-enum class FillType{
-  No_fill,
-  Fill
-}
+class VisualData{
 
-class VisualData : lgfx::LGFXBase{
-
-
-  
-  void drawPixel(int32_t x, int32_t y){
-    lgfx::LGFXBase::drawPixel(x, y);
-    lgfx::LGFXBase::drawString();
-    
-  }
 };
